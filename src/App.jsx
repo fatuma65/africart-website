07b1e9af22b24d06e04867c2ca9d0e1b9b52b04e
Aaron Mandela <aaronokullo82@gmail.com>
2024-10-07 17:13:55 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-import Header from "./Components/Header";
-
-const App = () => {
-  const current_theme = localStorage.getItem('current_theme');
-  const [theme, setTheme] = useState(current_theme ? current_theme : 'light');
-
-  useEffect(()=>{
-    localStorage.setItem('current_theme', theme)
-  },[theme])
-  return (
-    <div className={`container ${theme}`}>
-      <Header theme={theme} setTheme={setTheme} />
-    </div>
-  );
-};
-=======
 import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
+import About from "./Pages/About";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import ProductDetails from "./Pages/ProductDetails";
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/product/:id" element={<ProductDetails />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
+import { Link, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
 import About from "./Pages/About";
@@ -24,19 +31,27 @@ import Blog from "./Pages/Blog";
 import Contact from "./Pages/Contact";
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
 function App() {
+  const current_theme = localStorage.getItem("current_theme");
+  const [theme, setTheme] = useState(current_theme ? current_theme : "light");
+
+  useEffect(() => {
+    localStorage.setItem("current_theme", theme);
+  }, [theme]);
   return (
     <>
       <div className={`container ${theme}`}>
         <Header theme={theme} setTheme={setTheme} />
       </div>
-
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/blog" element={<Blog />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="login" element={<Login />} />
+       
       </Routes>
     </>
   );
