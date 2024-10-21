ebff97a92417cdfc1cc64b6f666e3449078e399d
Miriam Birungi <birungimiriam323@gmail.com>
2024-10-21 21:26:45 +0300

    add about us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,16 @@
+import AboutPage from "../components/about/AboutPage"
+import Footer from "../components/footer/Footer"
+import Navbar from "../components/Navbar"
+import Header from '../components/Header/Header'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <Header />
+    <Navbar />
+    <AboutPage />
+    <Footer />
+    </>
   )
 }
 
